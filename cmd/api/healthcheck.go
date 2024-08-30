bbca9b6c30ff560e990cee8950575db51c6e8a1f
Jack Jutzi <jackjutzi@Jacks-MBP.lan>
2024-08-30 14:26:51 -0400

    update responses to be JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Status: OK")
-	fmt.Println(w, "environment: %s\n", app.config.env)
-	fmt.Println(w, "version: %s\n", version)
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "failed to process the request", http.StatusInternalServerError)
+	}
 }
---
+++ cmd/api/movies.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"go-service/internal/data"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "The Matrix",
+		Runtime:   120,
+		Genres:    []string{"drama", "action", "comedy"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "failed to process the request", http.StatusInternalServerError)
+	}
+}
