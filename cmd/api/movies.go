99895a3df9e12bf61b1147b8b1f9bad99048bcf5
Jack Jutzi <jackjutzi@Jacks-MBP.lan>
2024-08-31 10:41:04 -0400

    improve error responses

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -16,7 +16,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	id, err := app.readIDParam(r)
 
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -32,7 +32,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "failed to process the request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
