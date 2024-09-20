76c40cf390cceefd1b5909859b3d353a468f2697
Jack Jutzi <jsjutzi@gmail.com>
2024-09-20 12:19:47 -0400

    add validation for fields provided on api request

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -12,4 +16,29 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+// Custom unmarshaler for Runtime type
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	value, err := strconv.ParseInt(parts[0], 10, 32)
+
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(value)
+
+	return nil
 }
